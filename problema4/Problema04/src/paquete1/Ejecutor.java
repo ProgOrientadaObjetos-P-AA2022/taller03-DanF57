67b84324d02851478407168f167dec7c04c3cb79
Daniel Eduardo Flores <danser2002@hotmail.com>
2022-05-01 10:10:11 -0500

    Código Completo

--- problema4/Problema04/src/paquete1/Ejecutor.java
+++ problema4/Problema04/src/paquete1/Ejecutor.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package paquete1;
+
+import paquete2.EquipoCelular;
 
 /**
  *
@@ -14,7 +12,67 @@ public class Ejecutor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Objetos
+        EquipoCelular ec1 = new EquipoCelular();
+        EquipoCelular ec2 = new EquipoCelular();
+        // Variables
+        String sO1 = "Android";
+        String pantalla1 = "20cm";
+        double costoInicial1 = 600;
+        double ivaPorcent1 = 12;
+        String direcMac1 = "1.0.210.1231";
+        String infoIMEI1 = "0.001.be.0e0";
+
+        String sO2 = "iOS";
+        String pantalla2 = "22cm";
+        double costoInicial2 = 1200;
+        double ivaPorcent2 = 16;
+        String direcMac2 = "1.0.210.13451";
+        String infoIMEI2 = "0.021.01.0e0";
+        // Metodos
+        ec1.establecerSO(sO1);
+        ec1.establecerPantalla(pantalla1);
+        ec1.establecerCostoInicial(costoInicial1);
+        ec1.establecerIvaPorcent(ivaPorcent1);
+        ec1.establecerDirecMac(direcMac1);
+        ec1.establecerInfoIMEI(infoIMEI1);
+        
+        ec2.establecerSO(sO2);
+        ec2.establecerPantalla(pantalla2);
+        ec2.establecerCostoInicial(costoInicial2);
+        ec2.establecerIvaPorcent(ivaPorcent2);
+        ec2.establecerDirecMac(direcMac2);
+        ec2.establecerInfoIMEI(infoIMEI2);
+        //Calcular
+        ec1.calcularIvaCostoInicial();
+        ec1.calcularCostoFinal();
+        ec2.calcularIvaCostoInicial();
+        ec2.calcularCostoFinal();
+        //Obtener
+        System.out.printf("Equipos Celulares\n\nEquipo Celular Uno\n"
+                + "Sistema Operativo: %s\nPantalla: %s\nCosto Inicial: %.2f\n"
+                + "Porcentaje IVA: %.1f\nIVA Resultante: %.2f\nDirección Mac: "
+                + "%s\nInformación IMEI: %s\nCosto Final: %.2f\n"
+                , ec1.obtenerSO()
+                , ec1.obtenerPantalla()
+                , ec1.obtenerCostoInicial()
+                , ec1.obtenerIvaPorcent()
+                , ec1.obtenerIvaCostoInicial()
+                , ec1.obtenerDirecMac()
+                , ec1.obtenerInfoIMEI()
+                , ec1.obtenerCostoFinal());
+        System.out.printf("\nEquipo Celular Dos\n"
+                + "Sistema Operativo: %s\nPantalla: %s\nCosto Inicial: %.2f\n"
+                + "Porcentaje IVA: %.1f\nIVA Resultante: %.2f\nDirección Mac: "
+                + "%s\nInformación IMEI: %s\nCosto Final: %.2f\n"
+                , ec2.obtenerSO()
+                , ec2.obtenerPantalla()
+                , ec2.obtenerCostoInicial()
+                , ec2.obtenerIvaPorcent()
+                , ec2.obtenerIvaCostoInicial()
+                , ec2.obtenerDirecMac()
+                , ec2.obtenerInfoIMEI()
+                , ec2.obtenerCostoFinal());
     }
-    
+
 }
--- problema4/Problema04/src/paquete2/EquipoCelular.java
+++ problema4/Problema04/src/paquete2/EquipoCelular.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package paquete2;
 
 /**
@@ -9,5 +5,77 @@ package paquete2;
  * @author Daniel
  */
 public class EquipoCelular {
-    
+
+    private String sO;
+    private String pantalla;
+    private String direcMac;
+    private String infoIMEI;
+    private double costoInicial;
+    private double ivaPorcent;
+    private double ivaCostoInicial;
+    private double costoFinal;
+
+    public void establecerSO(String c) {
+        sO = c;
+    }
+
+    public void establecerPantalla(String c) {
+        pantalla = c;
+    }
+
+    public void establecerDirecMac(String c) {
+        direcMac = c;
+    }
+
+    public void establecerInfoIMEI(String c) {
+        infoIMEI = c;
+    }
+
+    public void establecerCostoInicial(double c) {
+        costoInicial = c;
+    }
+
+    public void establecerIvaPorcent(double c) {
+        ivaPorcent = c;
+    }
+
+    public void calcularIvaCostoInicial() {
+        ivaCostoInicial = costoInicial * (ivaPorcent / 100);
+    }
+
+    public void calcularCostoFinal() {
+        costoFinal = ivaCostoInicial + costoInicial;
+    }
+
+    public String obtenerSO() {
+        return sO;
+    }
+
+    public String obtenerPantalla() {
+        return pantalla;
+    }
+
+    public String obtenerDirecMac() {
+        return direcMac;
+    }
+
+    public String obtenerInfoIMEI() {
+        return infoIMEI;
+    }
+
+    public double obtenerCostoInicial() {
+        return costoInicial;
+    }
+
+    public double obtenerIvaPorcent() {
+        return ivaPorcent;
+    }
+
+    public double obtenerIvaCostoInicial() {
+        return ivaCostoInicial;
+    }
+
+    public double obtenerCostoFinal() {
+        return costoFinal;
+    }
 }
